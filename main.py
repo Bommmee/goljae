4ba05a6893a3fcc914d1bf05099c07760a3703b3
Bomme <94340274+Bommmee@users.noreply.github.com>
2025-01-14 15:58:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 로컬 Next.js 서버
+    allow_origins=["*"],  # 로컬 Next.js 서버
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
